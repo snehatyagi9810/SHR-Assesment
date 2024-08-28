8903c0c4bda27c9dc63eb9dfcfccea3186a40577
Sneha Tyagi <Snehtyagi9810@gmail.com>
2024-08-28 18:29:03 +0530

    Initial Commit

---
+++ SHR Assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHR Assesment
+//
+//  Created by Sneh  on 28/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
